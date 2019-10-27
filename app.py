0578c3e0508e66109e38302d320c36ae6b5fef67
Max Zuo <max.zuo@gmail.com>
2019-10-27 02:54:20 -0400

    Created supplemental database for keeping track of balances:

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import requests
 import json
 import random
 from datetime import datetime
+import sqlite3
 
 
 valid = "abcdefghijklmnopqrstuvwxyz0123456789"
 apiKey = "7eef02ee968ed07e8f2b15b969e927f5"
 live_transactions = dict()
+
+db = "zappay.db"
 
 app = Flask(__name__)
 
@@ -15,7 +18,7 @@ app = Flask(__name__)
 def generate_transfer():
 	firstName = request.args.get("first_name", None)
 	lastName = request.args.get("last_name", None)
-	payee_id = request.args.get("customer_id", None)
+	payee_id = request.args.get("account_id", None)
 
 	amount = int(request.args.get("amount", None))
 	key = None
@@ -38,7 +41,8 @@ def check_transfer():
 	uid = request.args.get("uid", "null")
 
 	transaction = live_transactions.get(uid, None)
-	if None:
+	if transaction is None:
+		print("Check transfer error")
 		return {"ERROR": "no transfer associated with that uid"}
 	return {"payee": transaction['name'], "amount": transaction["amount"]}
 
@@ -46,36 +50,77 @@ def check_transfer():
 
 @app.route("/accept_transfer", methods=["GET"])
 def accept_transfer():
-	payer_id = request.args.get("customer_id", None)
+	payer_id = request.args.get("account_id", None)
 	uid = request.args.get("uid", "null")
+
 	transaction = live_transactions.get(uid, None)
-	if None:
+	if transaction is None:
+		print("ERROR", transaction)
+		print(live_transactions.keys(), uid)
 		return {"ERROR": "no transfer associated with that uid"}
 
 	data = {
 		"medium": "balance",
 		"payee_id": transaction["payee_id"],
+		"amount": transaction["amount"],
 		"transaction_date": datetime.today().strftime("%Y-%m-%d"),
 		"status": "pending",
 		"description": "string"
 	}
 
+	
+	database = sqlite3.connect(db)
+	c = database.cursor()
+	c.execute('SELECT balance FROM BALANCES WHERE account_id = ?;', [payer_id])
+	payer_balance = c.fetchall()[0][0]
+	c.execute('SELECT balance FROM BALANCES WHERE account_id = ?;', [transaction['payee_id']])
+	payee_balance = c.fetchall()[0][0]
+
+	if transaction["amount"] > payer_balance:
+		return {"ERROR:", "REQUESTED AMOUNT GREATER THAN TOTAL BALANCE"}
+
+	payee_balance += transaction["amount"]
+	payer_balance -= transaction["amount"]
+
+	c.execute('UPDATE BALANCES SET balance = ? WHERE account_id = ?;', [payer_balance, payer_id])
+	c.execute('UPDATE BALANCES SET balance = ? WHERE account_id = ?;', [payee_balance, transaction['payee_id']])
+
+	database.commit()
+	database.close()
+
 	url = "http://api.reimaginebanking.com/accounts/{}/transfers?key={}".format(payer_id, apiKey)
 	res = requests.post(url, json=data)
+
+	live_transactions.pop(uid, None)
+	print(res.json())
+
 
 	return res.json()
 
 
+# @app.route("/get_balance", methods=["GET"])
+# def get_balance():
+	# customerId = request.args.get("customerID", None)
+	# print(request.args.to_dict().keys())
+	# print(customerId)
+	# url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId, apiKey)
+	# account = requests.get(url).json()[0]
+	# print(account)
+	# balance = account.get('balance', None)
+	# return {"balance":balance}
+
 @app.route("/get_balance", methods=["GET"])
 def get_balance():
-	customerId = request.args.get("customerID", None)
-	print(request.args.to_dict().keys())
-	print(customerId)
-	url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId, apiKey)
-	account = requests.get(url).json()[0]
-	print(account)
-	balance = account.get('balance', None)
-	return {"balance":balance}
+	account_id = request.args.get("account_id", None)
+	database = sqlite3.connect(db)
+	c = database.cursor()
+	c.execute('SELECT balance FROM BALANCES WHERE account_id = ?;', [account_id])
+	balances = c.fetchall()
+	# database.commit()
+	database.close()
+	print(balances, balances[0][0])
+	return {"balance":balances[0][0]}
+
 
 @app.route("/create_account", methods=["GET"])
 def create_account():
@@ -111,17 +156,23 @@ def create_account():
 	if customer_id is None:
 		return {"error": "did not successfully create customer"}
 	url = "http://api.reimaginebanking.com/customers/{}/accounts?key={}".format(customer_id, apiKey)
+	balance = int(request.args.get("balance", "0", type=str))
 	account_data = {
 		"type": "Credit Card",
 		"nickname": "%s %s" % (firstName, lastName),
 		"rewards": 0,
-		"balance": int(request.args.get("balance", "0", type=str))
+		"balance": balance
 	}
 
 	account = requests.post(url, json=account_data)
 	print(account.json())
 	account_id = account.json().get("objectCreated").get("_id")
 	print(customer_id, account_id)
+	database = sqlite3.connect(db)
+	c = database.cursor()
+	c.execute('INSERT INTO BALANCES (account_id, balance) VALUES (?, ?);', [account_id, balance])
+	database.commit()
+	database.close()
 	return {"customer_id": customer_id, "account_id": account_id}
 
 
